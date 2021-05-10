e8751ff9d8d9f0ef1792872ba126b966ef79da81
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-05-10 16:17:27 -0400

    Solves first function successfully

--- functions.js
+++ functions.js
@@ -1,7 +1,30 @@
 const identity = function (value) {
   return value;
 };
-const first = function (array, n) {};
+
+//first function will return the first index if n is undefined or up until n index if defined
+const first = function (array, n) {
+  // declare a new array to be returned
+  let results = [];
+  // if n is undefined, simply return the first element of the array
+  if (n === undefined) {
+    return array[0];
+  }
+  // if n parameter is longer than array length, return all elements in array
+  else if (n > array.length) {
+    for (let i = 0; i < array.length; i++) {
+      results.push(array[i]);
+    }
+  } 
+  // if n is defined loop through until the n index
+  else {
+    for (let i = 0; i < n; i++) {
+      results.push(array[i]);
+    }
+  };
+  
+  return results;
+};
 
 const last = function (array, n) {};
 
